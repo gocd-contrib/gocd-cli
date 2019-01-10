5915aee481d141d41f171a5833fc12f88ec69c8d
Marques Lee <marques.lee@thoughtworks.com>
2019-01-09 17:27:28 -0800

    Add a terminal helper to specify whether or not commands require the use of `xargs` when piping STDIN

--- utils/terminal.go
+++ utils/terminal.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func StdoutOrDevNull() io.Writer {
@@ -24,4 +26,31 @@ func StderrOrDevNull() io.Writer {
 func HasShellPipe() bool {
 	fi, _ := os.Stdin.Stat()
 	return (fi.Mode() & os.ModeCharDevice) == 0
+}
+
+func UseXargsOverPipe(rawArgs []string) error {
+	if HasShellPipe() {
+		return &MustUseXargs{Invocation: rawArgs}
+	}
+	return nil
+}
+
+type MustUseXargs struct {
+	Invocation []string
+}
+
+func (e *MustUseXargs) Error() string {
+	command := strings.Join(e.Invocation, ` `)
+
+	f := strings.Join([]string{
+		"This command does not directly accept a shell pipe; perhaps you meant to use `xargs`?",
+		``,
+		`Example (*Nix):`,
+		"  `<piped-output> | xargs %s`",
+		``,
+		`Example (PowerShell):`,
+		"  `<piped-output> | %% { $_.split()[0] } | %% { %s $_ }`",
+	}, "\n") + "\n"
+
+	return fmt.Sprintf(f, command, command)
 }
