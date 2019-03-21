e3a9a5520ad9d161475544a9d6b12f5ce7f654c0
kierarad <kieraradman@gmail.com>
2019-03-20 19:42:39 -0700

    Add command for setting token auth

--- cmd/config/auth_basic.go
+++ cmd/config/auth_basic.go
@@ -7,7 +7,7 @@ import (
 
 var BasicAuthCmd = &cobra.Command{
 	Use:   "auth-basic <user> <pass>",
-	Short: "Configures basic authentication for API requests to the GoCd server instance",
+	Short: "Configures basic authentication for API requests to the GoCD server instance",
 	Long:  "This sets the basic authentication credentials for GoCD API requests used by this CLI tool.",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
