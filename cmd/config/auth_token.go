c43a79da838efe39cc2f640326f04ef55f4586ba
Marques Lee <marques.lee@thoughtworks.com>
2019-03-21 14:09:24 -0700

    `auth-token` accepts token through piped STDIN

--- cmd/config/auth_token.go
+++ cmd/config/auth_token.go
@@ -1,15 +1,22 @@
 package config
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"github.com/gocd-contrib/gocd-cli/utils"
 	"github.com/spf13/cobra"
 )
 
 var TokenAuthCmd = &cobra.Command{
-	Use:   "auth-token <token>",
+	Use:   "auth-token TOKEN",
 	Short: "Configures token authentication for API requests to the GoCD server instance",
-	Long:  "This sets Personal Access Token for GoCD API requests used by this CLI tool.",
-	Args:  cobra.ExactArgs(1),
+	Long:  "This sets the Personal Access Token for GoCD API requests made by this CLI tool.",
+	Example: strings.Trim(`
+  gocd config auth-token abcd1234             # sets the auth token to abcd1234
+  cat token.txt | gocd config auth-token -    # sets the token from STDIN; must pass in the "-" argument`, "\n"),
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		tokenAuth.Run(args)
 	},
@@ -20,7 +27,22 @@ var tokenAuth = &TokenAuthRunner{}
 type TokenAuthRunner struct{}
 
 func (su *TokenAuthRunner) Run(args []string) {
-	if err := conf().SetTokenAuth(args[0]); err != nil {
+	var token string
+	if utils.HasShellPipe() {
+		if `-` != args[0] {
+			utils.DieLoudly(1, `For piped input, you must specify "-" as the argument`)
+		}
+
+		if b, err := ioutil.ReadAll(os.Stdin); err != nil {
+			utils.DieLoudly(1, `Failed to read from STDIN; Cause: %v`, err)
+		} else {
+			token = strings.TrimSpace(string(b))
+		}
+	} else {
+		token = args[0]
+	}
+
+	if err := conf().SetTokenAuth(token); err != nil {
 		utils.AbortLoudly(err)
 	}
 }
