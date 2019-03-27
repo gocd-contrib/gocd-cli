630fbf887fbb20ff4bbaf750d3029769bf228399
Marques Lee <marques.lee@thoughtworks.com>
2019-03-27 13:59:18 -0700

    Remove export alias - this is not a first-level subcommand.
    Also fixes validargs setting on config subcommand

--- cmd/config/root.go
+++ cmd/config/root.go
@@ -9,7 +9,7 @@ var RootCmd = &cobra.Command{
 	Use:       "config",
 	Aliases:   []string{"cf"},
 	Short:     "GoCD CLI configuration",
-	ValidArgs: []string{"auth-basic", "server-url", "help", "clear"}, // bash-completion
+	ValidArgs: []string{"auth-basic", "server-url", "help", "delete"}, // bash-completion
 }
 
 // convenvience method so subcommands don't need to import cfg
