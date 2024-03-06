980ac5d832b16871fad6319f9335e47a5c6e2277
Chad Wilson <chadw@thoughtworks.com>
2024-03-06 23:16:34 +0800

    Build only for modern commonly used platforms

--- build.sh
+++ build.sh
@@ -67,14 +67,11 @@ fi
 
 if [[ "true" = "$multiplatform" ]]; then
   platforms=(
+    darwin/arm64
     darwin/amd64
-    freebsd/386
-    freebsd/amd64
-    linux/386
     linux/amd64
-    linux/arm
     linux/arm64
-    windows/386
+    windows/arm64
     windows/amd64
   )
 
